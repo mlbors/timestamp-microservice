5b32e56d18fea668428e2ae7509a503bde718e6a
mlbors <matyas.bors@gmail.com>
2017-09-17 18:28:07 +0200

    Fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http')
 const url = require('url')
 
 const hostname = '0.0.0.0';
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' })
@@ -10,4 +10,4 @@ const server = http.createServer((req, res) => {
   res.end();
 }).listen(port, hostname, function(){
   console.log('Server running at http://${' + hostname + '}:${' + port + '}/');
-});
+})
