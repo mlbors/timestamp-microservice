42f0734e05a1969c3e40031ccf80825aa27a89a4
mlbors <matyas.bors@gmail.com>
2017-09-17 18:20:14 +0200

    change port

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const http = require('http')
 const url = require('url')
 
 const hostname = '0.0.0.0';
-const port = process.env.PORT;
+const port = process.env.port || 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' })
   res.write('Hello World!');
   res.end();
-}).listen(port, hostname, function(){
+})
+
+server.listen(port, hostname, function(){
   console.log('Server running at http://${' + hostname + '}:${' + port + '}/');
 });
