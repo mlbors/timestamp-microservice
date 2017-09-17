be3749514a1c7dd241d2c69618559f95b8461f84
mlbors <matyas.bors@gmail.com>
2017-09-17 12:06:05 +0200

    Change package.json

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const http = require('http')
 const url = require('url')
 
+const hostname = '0.0.0.0';
+const port = process.env.PORT;
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' })
   res.write('Hello World!');
   res.end();
-})
-
-server.listen(8080)
+}).listen(port, hostname, function(){
+  console.log('Server running at http://${' + hostname + '}:${' + port + '}/');
+});
